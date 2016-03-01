3ea63484ba286c4aede26410f274d6470adbc179
Oscar Loor <dwquito@hotmail.com>
2016-03-01 09:20:04 -0500

    Se cambio para obtener los datos despues de iniciar sesión

--- www/js/Controladores/inicioSesion.js
+++ www/js/Controladores/inicioSesion.js
@@ -1,23 +1,23 @@
-aplicacion.controller('iniciarSesionControlador', ['$scope', '$cordovaOauth', '$http','$ionicPlatform', function ($scope, $cordovaOauth, $http,$ionicPlatform) {
- var token = '2981244912.4414c79.632d91df27c64f1797d2362c0279a653';
-
-
-
- var url = "https://api.instagram.com/v1/users/self/?access_token=2981244912.4414c79.632d91df27c64f1797d2362c0279a653&callback=JSON_CALLBACK";
-
- $http.jsonp(url).success(function (data) {
-  console.log(data.data)
-  $scope.nombre = data.data.full_name
-  $scope.imagenDePerfil = data.data.profile_picture
-  $scope.idUsuario = data.data.id
- });
-
-
+aplicacion.controller('iniciarSesionControlador', ['$scope', '$cordovaOauth', '$http', '$ionicPlatform', function ($scope, $cordovaOauth, $http, $ionicPlatform) {
  $scope.login = function () {
   var igClientId = "4414c79c1d9d4ee9b027bf208331ccd1";
   $ionicPlatform.ready(function () {
    $cordovaOauth.instagram(igClientId, ["scope=basic"]).then(function (result) {
-    $scope.respuesta = result.access_token; //Obtengo el token
+    var token = result.access_token; //Obtengo el token
+
+    //Creo la URL con el token recibido
+    var url = "https://api.instagram.com/v1/users/self/?access_token=" + token + "&callback=JSON_CALLBACK";
+
+    //Hago la llamada a la API de Instagram
+    $http.jsonp(url).success(function (data) {
+     console.log(data.data)
+     $scope.nombre = data.data.full_name
+     $scope.imagenDePerfil = data.data.profile_picture
+     $scope.idUsuario = data.data.id
+    });
+
+
+
    }, function (error) {
     $scope.respuesta = error;
    });
