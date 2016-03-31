f11b2263862107b48fd621d986a087a5484c6a41
Oscar Loor <dwquito@hotmail.com>
2016-03-31 11:25:59 -0500

    Se agregaron archivos de respaldo para cambiar rapidamente entre ambiente de pc y app

---
+++ www/js/Controladores/menuLateralControladorParaPruebas.js
@@ -0,0 +1,89 @@
+aplicacion.controller('menuLateralControlador', ['$scope','$state','parametrosUsuarioFactory','$cordovaSQLite','$ionicPopup','$ionicHistory','$firebaseObject',function ($scope,$state,parametrosUsuarioFactory,$cordovaSQLite,$ionicPopup,$ionicHistory, $firebaseObject) {
+
+  //Escucho si se realiza algun cambio en los datos de la factory
+  $scope.$on("datosActualizados", function(){
+    $scope.nombre = parametrosUsuarioFactory.obtenerNombre();
+    $scope.imagenDePerfil = parametrosUsuarioFactory.obtenerImagenDePerfil();
+    if(parametrosUsuarioFactory.obtenerIdUsuario().length!=0){
+      var referenciaUsuario = new Firebase("https://servidorbmn.firebaseio.com/usuarios/" + parametrosUsuarioFactory.obtenerIdUsuario());
+      var lecturaReferenciaUsuario = $firebaseObject(referenciaUsuario);
+      $scope.datosPromoPoints = {};
+      lecturaReferenciaUsuario.$loaded()
+        .then(function(x) {
+
+          lecturaReferenciaUsuario.$bindTo($scope, "datosPromoPoints").then(function() {
+            //console.log($scope.datosPromoPoints.promoPoints);
+          });
+
+        })
+    }
+  });
+
+
+
+ $scope.cerrarSesion = function (){
+   //Borro los datos de factory
+   parametrosUsuarioFactory.actualizarNombre('');
+   parametrosUsuarioFactory.actualizarToken('');
+   parametrosUsuarioFactory.actualizarPais('');
+   parametrosUsuarioFactory.actualizarIdUsuario('');
+   parametrosUsuarioFactory.actualizarImagenDePerfil('');
+
+/*Eliminar esto cuando ya este en el dispositivo*/
+   $ionicHistory.nextViewOptions({
+     disableBack: true
+   });
+  $state.go('iniciarSesion'); //Redirige hacia una ruta
+/*Eliminar esto cuando ya este en el dispositivo*/
+
+  //  //Borro los datos de la base de datos
+  //  //Creo la sesion con el usuario
+  //  var db = $cordovaSQLite.openDB({
+  //    name: "com.datosBMN"
+  //  });
+  //  //Borro todos los registros de la tabla para asegurar siempre un registro
+  //  $cordovaSQLite.execute(db, "DELETE FROM usuario").then(function(res) {
+   //
+  //    $ionicPopup.confirm({
+  //        title: 'Cerrar Sesión',
+  //        content: 'La sesión se ha cerrado correctamente'
+  //      })
+  //      .then(function(result) {
+  //        $ionicHistory.nextViewOptions({
+  //          disableBack: true
+  //        });
+  //       $state.go('iniciarSesion'); //Redirige hacia una ruta
+  //      });
+  //  }, function(err) {
+   //
+  //    $ionicPopup.confirm({
+  //        title: 'Error al cerrar la sesión',
+  //        content: JSON.stringify(err)
+  //      })
+  //      .then(function(result) {
+  //      });
+  //  });//execute
+
+ };
+$scope.promoPointsAbrir = function(){
+  $state.go('promoPointsMenu'); //Redirige hacia una ruta
+ }
+
+$scope.pointsPlusAbrir = function(){
+  $state.go('pointsPlusMenu'); //Redirige hacia una ruta
+ }
+
+ $scope.promocionarFotosAbrir = function(){
+  $state.go('promocionarFotos'); //Redirige hacia una ruta
+ }
+
+ $scope.promocionarFollowsAbrir = function(){
+  $state.go('promocionarFollows'); //Redirige hacia una ruta
+ }
+
+ $scope.pointsPlusCorporateAbrir = function(){
+  $state.go('pointsPlusCorporateMenu'); //Redirige hacia una ruta
+ }
+
+
+}])
